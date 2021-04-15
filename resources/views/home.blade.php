ce70096d4b3e98ead312a16c0a92f61bf1963007
reatweb <reatweb@gmail.com>
2021-04-15 14:44:05 +0300

    setup tailwindcss and vue

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Weather App</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="/js/app.js" defer></script>
+</head>
+<body class="bg-blue-500">
+    <div id="app">
+        <weather-card></weather-card>
+    </div>
+</body>
+</html>
